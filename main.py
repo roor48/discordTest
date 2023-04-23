094f4c9dbd2a17c1f9aaf6a2724aad64543f8220
roor48 <kmgoio@naver.com>
2023-04-23 14:44:48 +0900

    메세지 보내는 코드

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello")
-a = 123
-print(a)
+import requests
+discord = "https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP"
+headers = {
+    'Content-Type':'application/json',
+}
+data = '{"content":"asd!"}'
+response = requests.post(discord, headers=headers, data=data)
