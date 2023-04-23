6c9f5c369311ec8bb28e041336b18028a230e14d
roor48 <kmgoio@naver.com>
2023-04-23 18:27:12 +0900

    gitingnore 생성

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
 from discord_webhook import DiscordWebhook
 
-webhookUrl = 'https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP'
+
+with open("./apiKey.json", 'r') as file:
+    webhookUrl = json.load(file)
 webhook = DiscordWebhook(url=webhookUrl, content = ':toolbox:')
 response = webhook.execute()
