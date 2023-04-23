870bd36a47bbe0c08b7bcfda8ec836c36ddb3079
roor48 <kmgoio@naver.com>
2023-04-23 16:07:28 +0900

    왕우

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from discord_webhook import DiscordWebhook
 
 webhookUrl = 'https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP'
-webhook = DiscordWebhook(url=webhookUrl, content = '||꼽구이 멍청이||')
+webhook = DiscordWebhook(url=webhookUrl, content = ':toolbox:')
 response = webhook.execute()
