49e2b555f940651b58ad8216878195b714fe3c32
roor48 <kmgoio@naver.com>
2023-04-23 16:00:05 +0900

    asdf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import requests
-discord = "https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP"
-headers = {
-    'Content-Type':'application/json',
-}
-data = '{"content":"asd!"}'
-response = requests.post(discord, headers=headers, data=data)
+from discord_webhook import DiscordWebhook
+
+webhookUrl = 'https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP'
+webhook = DiscordWebhook(url=webhookUrl, content = '||꼽구이 멍청이||')
+response = webhook.execute()
