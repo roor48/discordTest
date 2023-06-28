7997930fbec90d2b1b455fc993da0651fcba3014
roor48 <kmgoio@naver.com>
2023-06-28 15:04:04 +0900

    asdf

--- Webmain.py
+++ Webmain.py
@@ -1,4 +1,5 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url=open(r"./apiKey.txt", 'r').readline(), content = 'Hello')
+apiKey = 'https://discord.com/api/webhooks/1099561516845510676/csJKKoGLDXlZV-6rhvn8DX_p0pECVcw-D6Ki_t7WPquFlO2GINV7kWbzlccGF05IU-xP'
+webhook = DiscordWebhook(url=open(apiKey, 'r').readline(), content = 'Hello')
 response = webhook.execute()
